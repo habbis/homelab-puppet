afdb32d08d2aa72963c194ae367876653d94b1cf
habbis <habbis@habbestad.org>
2025-09-27 16:50:27 +0200

    testing classs

--- modules/server_lite/manifests/init.pp
+++ modules/server_lite/manifests/init.pp
@@ -4,7 +4,7 @@ class server_lite {
   include server_lite::resolv_conf
   include server_lite::bash_profile
   include server_lite::packages
-  #include server_lite::ansible_user
+  include server_lite::ansible_user
   #include server_lite::hosts
   #include server_lite::motd
   #include server_lite::cron::puppet_restart
