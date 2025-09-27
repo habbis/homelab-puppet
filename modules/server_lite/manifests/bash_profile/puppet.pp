236abffe3f87565b91557daf0083c53d14e91aea
habbis <habbis@habbestad.org>
2025-09-27 16:44:54 +0200

    fixing bash_profile class

--- modules/server_lite/manifests/bash_profile/puppet.pp
+++ modules/server_lite/manifests/bash_profile/puppet.pp
@@ -1,9 +1,9 @@
 # Class to manage puppet profile.d
-class server_lite::bash_profile::proxy {
+class server_lite::bash_profile::puppet {
   file { '/etc/profile.d/puppet.sh':
     owner  => root,
     group  => root,
     mode   => '0755',
-    source => 'puppet:///modules/server_lite/bash_profile/puppet.sh'
+    source => 'puppet:///modules/server_lite/bash_profile/puppet.sh',
   }
 }
