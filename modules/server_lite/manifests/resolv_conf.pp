fc5b1b845b1c50aaa41b7174936d9460cf77f57d
habbis <habbis@habbestad.org>
2025-09-27 16:33:42 +0200

    fixing resolv_conf class

--- modules/server_lite/manifests/resolv_conf.pp
+++ modules/server_lite/manifests/resolv_conf.pp
@@ -12,6 +12,6 @@ class server_lite::resolv_conf {
     owner  => root,
     group  => root,
     mode   => '0644',
-    source => template('server_lite/resolv.conf.erb'),
+    source => template('server_lite/resolv_conf/resolv.conf.erb'),
   }
 }
