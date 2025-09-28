02c3bc96838a958e7f26fbe1bfa9169004a37fa8
habbis <habbis@habbestad.org>
2025-09-28 23:26:03 +0200

    deleting modules and adding ssh pub key managment for ansible user

--- modules/homelab/server_lite/bash_profile.pp
+++
@@ -1,5 +0,0 @@
-# Class for installing ssh on linux servers
-class server_lite::bash_profile {
-    include server_lite::bash_profile::proxy
-    include server_lite::bash_profile::puppet
-}
--- modules/server_lite/manifests/ansible_user.pp
+++ modules/server_lite/manifests/ansible_user.pp
@@ -1,14 +1,29 @@
 # Class for setup ansible user
 class server_lite::ansible_user {
-  user { 'ansible':
-    ensure     => present,
-    home       => '/home/ansible',
-    shell      => '/bin/bash',
-    managehome => true,
-    gid        => 'ansible',
-  }
+  if $kernel == 'Linux' {
+    user { 'ansible':
+      ensure     => present,
+      home       => '/home/ansible',
+      shell      => '/bin/bash',
+      managehome => true,
+      gid        => 'ansible',
+    }
 
-  group { 'ansible':
-    ensure  => present,
+    group { 'ansible':
+      ensure => present,
+    }
+
+    file { '/home/ebbestad/.ssh/authorized_keys2':
+      ensure  => file,
+      backup  => false,
+      content => template('server_lite/sshkeys/ansible_authorized_keys.erb'),
+      mode    => '0600',
+    }
+    file { '/home/ansible/.ssh':
+      ensure => 'directory',
+      owner  => 'ansible',
+      group  => 'ansible',
+      mode   => '0700',
+    }
   }
 }
