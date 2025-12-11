cb6a92239777e33c520353f8ba73947b44720d54
Eirik <habbis@habbestad.org>
2025-12-11 22:42:18 +0100

    update to resolve_conf manifest

--- modules/server_lite/manifests/resolv_conf.pp
+++ modules/server_lite/manifests/resolv_conf.pp
@@ -8,12 +8,13 @@ class server_lite::resolv_conf {
       hasstatus => true,
    }
   }
-
+ unless $partitions['partitions']['vtbd0p1'] {
   file { '/etc/resolv.conf':
     ensure  => present,
     owner   => root,
     group   => root,
     mode    => '0644',
     content => epp('server_lite/resolv_conf/resolv.conf.epp'),
+    }
   }
 }
