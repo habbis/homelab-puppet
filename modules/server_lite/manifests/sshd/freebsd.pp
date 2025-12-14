0612f9cf953def0412d26568cfc1619f17608db1
Eirik <habbis@habbestad.org>
2025-12-14 12:45:28 +0100

    fixing pre-commit

---
+++ modules/server_lite/manifests/sshd/freebsd.pp
@@ -0,0 +1,25 @@
+# Class to install nrpe on debian and ubuntu
+class server_lite::sshd::freebsd inherits server_lite::sshd {
+
+  service {
+    'sshd':
+      ensure     => running,
+      enable     => true,
+      hasstatus  => true,
+      hasrestart => true;
+  }
+
+  file { '/etc/ssh/sshd_config':
+    ensure  => 'present',
+    owner   => root,
+    group   => wheel,
+    mode    => '0600',
+    content => template('server_lite/sshd/freebsd/sshd_config.erb');
+  }
+
+  exec {
+    'ssh_restart':
+      command     => '/usr/bin/systemctl restart sshd',
+      refreshonly => true;
+  }
+}
