8c54bcdd223d4cc963c9124340b8613ef2c208c5
Eirik <habbis@habbestad.org>
2025-12-14 14:03:15 +0100

    adding bash to ansible user for freebsd

--- modules/server_lite/manifests/ansible_user.pp
+++ modules/server_lite/manifests/ansible_user.pp
@@ -27,4 +27,57 @@ class server_lite::ansible_user {
       mode    => '0600',
     }
   }
+  if $facts['os'] == 'FreeBSD' {
+    user { 'ansible':
+      ensure     => present,
+      home       => '/home/ansible',
+      shell      => '/usr/local/bin/bash',
+      managehome => true,
+      gid        => 'ansible',
+    }
+
+    group { 'ansible':
+      ensure => present,
+    }
+
+    file { '/home/ansible/.ssh':
+      ensure => 'directory',
+      owner  => 'ansible',
+      group  => 'ansible',
+      mode   => '0700',
+    }
+
+    file { '/home/ansible/.ssh/authorized_keys2':
+      ensure  => file,
+      backup  => false,
+      content => template('server_lite/sshkeys/authorized_keys.erb'),
+      mode    => '0600',
+    }
+    file { '/home/ansible/.bash_history':
+      ensure => present,
+      owner  => 'ansible',
+      group  => 'ansible',
+      mode   => '0644',
+    }
+    file { '/home/ansible/.bash_logout':
+      ensure => present,
+      owner  => 'ansible',
+      group  => 'ansible',
+      mode   => '0644',
+    }
+  file { '/home/ansible/.bash_profile':
+    ensure => present,
+    owner  => 'ansible',
+    group  => 'ansible',
+    mode   => '0644',
+    source => 'puppet:///modules/server_lite/bash_files/freebsd/.bash_profile'
+    }
+  file { '/home/ansible/.bashrc':
+    ensure => present,
+    owner  => 'ansible',
+    group  => 'ansible',
+    mode   => '0644',
+    source => 'puppet:///modules/server_lite/bash_files/freebsd/.bashrc'
+    }
+  }
 }
