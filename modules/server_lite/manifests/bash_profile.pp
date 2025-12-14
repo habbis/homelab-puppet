9972bd049bc6f866f7b072b3a6bc8ce0aaad260a
Eirik <habbis@habbestad.org>
2025-12-14 15:00:59 +0100

    adding alias to bash_profile

--- modules/server_lite/manifests/bash_profile.pp
+++ modules/server_lite/manifests/bash_profile.pp
@@ -1,7 +1,8 @@
-# Class for installing ssh on unix servers
+# Class for setup shell profile on unix servers
 class server_lite::bash_profile {
   if $facts['kernel'] == 'Linux' {
   include server_lite::bash_profile::proxy
   include server_lite::bash_profile::puppet
+  include server_lite::bash_profile::shell_alias
   }
 }
