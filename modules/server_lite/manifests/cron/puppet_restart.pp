93dda4e0027c8480f64bf050d6917baf816daf1e
Eirik <habbis@habbestad.org>
2025-12-16 00:18:09 +0100

    change to cron manifest

--- modules/server_lite/manifests/cron/puppet_restart.pp
+++ modules/server_lite/manifests/cron/puppet_restart.pp
@@ -1,6 +1,6 @@
 # Class to restart puppet agent.
 class server_lite::cron::puppet_restart {
-  unless $facts['os']['name'] == 'windows' {
+  if $facts['kernel'] == 'Linux' {
     cron { 'puppet-restart':
       ensure      => 'present',
       environment => 'PATH=/bin:/sbin:/usr/bin:/usr/sbin',
@@ -8,6 +8,17 @@ class server_lite::cron::puppet_restart {
       user        => 'root',
       hour        => '22',
       minute      => fqdn_rand(59);
+      }
+
+  if $facts['kernel'] == 'FreeBSD' {
+    cron { 'puppet-restart':
+      ensure      => 'present',
+      environment => 'PATH=/bin:/sbin:/usr/bin:/usr/sbin',
+      command     => 'service puppet restart > /dev/null',
+      user        => 'root',
+      hour        => '22',
+      minute      => fqdn_rand(59);
+      }
     }
   }
 }
