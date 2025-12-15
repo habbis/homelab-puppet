9aed372c332aad6b0ccf0fdb2ac9d4e304f63e58
Eirik <habbis@habbestad.org>
2025-12-15 21:50:04 +0100

    fixing logic bug

--- modules/server_lite/manifests/resolv_conf.pp
+++ modules/server_lite/manifests/resolv_conf.pp
@@ -4,7 +4,7 @@ class server_lite::resolv_conf {
   $disk_path = '/dev/vtbd0'
   $disk_exists = find_file($disk_path)
   $disk_path2 = '/dev/vda'
-  $disk_exists = find_file($disk_path2)
+  $disk_exists2 = find_file($disk_path2)
 
   if $facts['kernel'] == 'Linux' {
   service {
@@ -15,7 +15,7 @@ class server_lite::resolv_conf {
     }
   }
   if !$disk_exists {
-  } elsif !$disk_exists {
+  } elsif !$disk_exists2 {
   } elsif $facts['os']['family'] != 'FreeBSD' {
   file { '/etc/resolv.conf':
     ensure  => present,
