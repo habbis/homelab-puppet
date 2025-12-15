05f4aa6b44afcc2d550b55f918af158d1439d382
Eirik <habbis@habbestad.org>
2025-12-15 23:11:39 +0100

    fixing logic in sshd manifest

--- modules/server_lite/manifests/sshd/freebsd.pp
+++ modules/server_lite/manifests/sshd/freebsd.pp
@@ -20,12 +20,14 @@ class server_lite::sshd::freebsd inherits server_lite::sshd {
   exec {
     'ssh_enable':
       command => '/usr/sbin/sysrc sshd_enable="YES"',
+      path    => ['/bin','/usr/bin', '/usr/sbin'],
       unless  => 'grep ^sshd /etc/rc.conf 2>/dev/null';
   }
 
   exec {
     'ssh_restart':
       command     => '/usr/sbin/service sshd restart',
+      path        => ['/bin','/usr/bin', '/usr/sbin'],
       subscribe   => File['/etc/ssh/sshd_config'],
       refreshonly => true,
   }
