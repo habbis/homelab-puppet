423fee68dbd7662212a6a6849095beffe2a7d293
Eirik <habbis@habbestad.org>
2025-12-16 13:34:01 +0100

    change to hosts manifest

--- modules/server_lite/manifests/hosts.pp
+++ modules/server_lite/manifests/hosts.pp
@@ -1,12 +1,29 @@
 #basic setup for /etc/hosts
 class server_lite::hosts {
-  if $facts['kernel'] == 'Linux' {
+
+  $disk_path = '/dev/vtbd0'
+  $disk_exists = find_file($disk_path)
+  $disk_path2 = '/dev/vda'
+  $disk_exists2 = find_file($disk_path2)
+
+
+  if !$disk_exists {
+  } elsif !$disk_exists2 {
+  } elsif $facts['kernel'] == 'Linux' {
   file {
     '/etc/hosts':
       mode    => '0644',
       owner   => root,
       group   => root,
       content => template('server_lite/etc_hosts/linux/hosts.erb');
+    }
+  } elsif $facts['os']['family'] != 'FreeBSD' {
+  file {
+    '/etc/hosts':
+      mode    => '0644',
+      owner   => root,
+      group   => root,
+      content => template('server_lite/etc_hosts/freebsd/hosts.erb');
     }
   }
 }
