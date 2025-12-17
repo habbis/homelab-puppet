bbcec12bc710c554bf7a521404737a5671b5df60
Eirik <habbis@habbestad.org>
2025-12-17 15:05:06 +0100

    change to bash_profile manifest

--- modules/server_lite/manifests/bash_profile.pp
+++ modules/server_lite/manifests/bash_profile.pp
@@ -1,6 +1,15 @@
 # Class for setup shell profile on unix servers
 class server_lite::bash_profile {
-  if $facts['kernel'] == 'Linux' {
+
+  $disk_path = '/dev/vtbd0'
+  $disk_exists = find_file($disk_path)
+  $disk_path2 = '/dev/vda'
+  $disk_exists2 = find_file($disk_path2)
+
+  if !$disk_exists {
+  } elsif !$disk_exists2 {
+  } elsif $facts['os']['family'] != 'FreeBSD' {
+  } elsif $facts['kernel'] == 'Linux' {
   include server_lite::bash_profile::proxy
   include server_lite::bash_profile::puppet
   include server_lite::bash_profile::shell_alias
