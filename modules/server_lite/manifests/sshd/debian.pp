be5f45e8bba157260b17744dce30a90a87b1be43
Eirik <habbis@habbestad.org>
2025-12-17 15:37:07 +0100

    change to sshd/debian manifest

--- modules/server_lite/manifests/sshd/debian.pp
+++ modules/server_lite/manifests/sshd/debian.pp
@@ -13,13 +13,23 @@ class server_lite::sshd::debian inherits server_lite::sshd {
       hasrestart => true;
   }
 
+  if $facts['os']['release']['major'] == '13' {
   file { '/etc/ssh/sshd_config':
     ensure  => 'present',
     owner   => root,
     group   => root,
     mode    => '0600',
-    content => template('server_lite/sshd/debian/sshd_config.erb');
-  }
+    content => template('server_lite/sshd/debian/sshd_config_deb13.erb');
+    }
+  } elsif $facts['os']['release']['major'] == '12' {
+  file { '/etc/ssh/sshd_config':
+    ensure  => 'present',
+    owner   => root,
+    group   => root,
+    mode    => '0600',
+    content => template('server_lite/sshd/debian/sshd_config_deb12.erb');
+      }
+    }
 
   exec {
     'ssh_restart':
