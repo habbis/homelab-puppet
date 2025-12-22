bcb92fde197fc02e273684d1397697145ce17462
Eirik <habbis@habbestad.org>
2025-12-22 14:23:22 +0100

    update to patching facts

--- modules/patching/facts.d/patch_policy.sh
+++ modules/patching/facts.d/patch_policy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-what="$(grep -o '[0-9]*' /etc/patching/patch_policy)"
+if [ -d /etc/patching ]; then
+        what="$(grep -o '[0-9]*' /etc/patching/patch_policy)"
+
+fi
 
 if [ ! -n "$what" ]; then
         what="none"
