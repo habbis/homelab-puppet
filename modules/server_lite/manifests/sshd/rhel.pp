c54d0fb6372e18b92c5716a80b3adad29e5a75f6
ebbestad <ebbestad@hf-linbast2.no.habbfarm.net>
2026-05-17 11:08:59 +0200

    fixing bug in sshd class

--- modules/server_lite/manifests/sshd/rhel.pp
+++ modules/server_lite/manifests/sshd/rhel.pp
@@ -1,14 +1,5 @@
 # Class to deploy sshd config
 class server_lite::sshd::rhel inherits server_lite::sshd {
-    service {
-      'sshd':
-        ensure     => running,
-        require    => Package['openssh-server'],
-        enable     => true,
-        hasstatus  => true,
-        hasrestart => true;
-    }
-
     file { '/etc/ssh/sshd_config':
       ensure  => 'present',
       owner   => root,
