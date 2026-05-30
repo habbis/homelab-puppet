124213e6757b9ce3c1213c4fc9261c0abbce6e4c
ebbestad <ebbestad@hf-linbast2.no.habbfarm.net>
2026-05-31 01:20:17 +0200

    fixing import in bash class

--- modules/server_lite/manifests/bash_profile.pp
+++ modules/server_lite/manifests/bash_profile.pp
@@ -1,15 +1,10 @@
 # Class for setup shell profile on unix servers
 class server_lite::bash_profile {
 
-  $disk_path = '/dev/vtbd0'
-  $disk_exists = find_file($disk_path)
-  $disk_path2 = '/dev/vda'
-  $disk_exists2 = find_file($disk_path2)
-
-  if !$disk_exists {
-  } elsif !$disk_exists2 {
-  } elsif $facts['os']['family'] != 'FreeBSD' {
-  } elsif $facts['kernel'] == 'Linux' {
+  if $facts['disks'] != 'vtbd0' and $facts['kernel'] == 'FreeBSD' {
+  include server_lite::bash_profile::proxy
+    }
+  if $facts['disks'] != 'vda' and $facts['kernel'] == 'Linux' {
   include server_lite::bash_profile::proxy
   include server_lite::bash_profile::puppet
   include server_lite::bash_profile::shell_alias
