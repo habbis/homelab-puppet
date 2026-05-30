a4b3668f20e02e3254571cd307bae639da86f93c
ebbestad <ebbestad@hf-linbast2.no.habbfarm.net>
2026-05-30 23:36:24 +0200

    fixing import in sshd.pp

--- modules/server_lite/manifests/sshd.pp
+++ modules/server_lite/manifests/sshd.pp
@@ -2,16 +2,22 @@
 class server_lite::sshd {
   if $facts['kernel'] == 'Linux' {
   include server_lite::sshd::linux_install_sshd
-  }
+}
 
-  case $facts['os']['family'] {
-    'RedHat': { include server_lite::sshd::rhel }
-    'Debian': { include server_lite::sshd::debian }
-    'FreeBSD': { include server_lite::sshd::freebsd }
-    default: {
-      include server_lite::sshd::rhel
-    }
-  }
+if $facts['os']['family'] == 'RedHat' {
+  include server_lite::sshd::rhel
+
+}
+
+if $facts['os']['family'] == 'Debian' {
+  include server_lite::sshd::debian
+
+}
+
+if $facts['os']['family'] == 'FreeBSD' {
+  include server_lite::sshd::freebsd
+
+}
 
   if $facts['kernel'] == 'Linux' {
   include server_lite::sshd::linux_start_sshd
