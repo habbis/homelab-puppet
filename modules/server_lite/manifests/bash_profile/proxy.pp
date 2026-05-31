a11a00b52df33d29806c26936d64e7e324678562
ebbestad <ebbestad@hf-linbast2.no.habbfarm.net>
2026-05-31 23:56:09 +0200

    adding logic for freebsd

--- modules/server_lite/manifests/bash_profile/proxy.pp
+++ modules/server_lite/manifests/bash_profile/proxy.pp
@@ -1,9 +1,19 @@
 # Class to manage gloabal proxy setting
 class server_lite::bash_profile::proxy {
+if  $facts['kernel'] == 'Linux' {
   file { '/etc/profile.d/shell_alias.sh':
     owner  => root,
     group  => root,
     mode   => '0755',
     source => 'puppet:///modules/server_lite/bash_profile/shell_alias.sh'
+    }
+  }
+if  $facts['os']['family'] =='FreeBSD' {
+  file { '/etc/profile.d/shell_alias.sh':
+    owner  => root,
+    group  => wheel,
+    mode   => '0755',
+    source => 'puppet:///modules/server_lite/bash_profile/shell_alias.sh'
+    }
   }
 }
