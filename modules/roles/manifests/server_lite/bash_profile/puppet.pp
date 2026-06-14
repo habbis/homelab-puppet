83fd55b2e23b8c27abe76635bd3ac5dd78830b6e
ebbestad <ebbestad@hf-linbast2.no.habbfarm.net>
2026-06-14 15:28:24 +0200

    fixing path

--- modules/roles/manifests/server_lite/bash_profile/puppet.pp
+++ modules/roles/manifests/server_lite/bash_profile/puppet.pp
@@ -4,6 +4,6 @@ class roles::server_lite::bash_profile::puppet {
     owner  => root,
     group  => root,
     mode   => '0755',
-    source => 'puppet:///modules/server_lite/bash_profile/puppet.sh',
+    source => 'puppet:///modules/bash_profile/puppet.sh',
   }
 }
