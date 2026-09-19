3bbf26a8309bcd9cbf40dbfda7758e8947d86596
ebbestad <ebbestad@hf-linbast2.no.habbfarm.net>
2026-09-19 12:49:45 +0200

    update puppet class

--- modules/roles/manifests/rsync_remote_server.pp
+++ modules/roles/manifests/rsync_remote_server.pp
@@ -10,12 +10,32 @@ class roles::rsync_remote_server (
   $local_destination = $roles::rsync_copy::local_destination,) {
 
   if $run_as_user == 'root' {
+    exec { 'eval-ssh-agent':
+      path    => ['/bin/', '/usr/bin/'],
+      command => 'eval $(ssh-agent)',
+      user    => 'root',
+    }
+    exec { 'add-ssh-agent':
+      path    => ['/bin/', '/usr/bin/'],
+      command => 'ssh-add',
+      user    => 'root',
+    }
     exec { 'rsync-remote-dir':
       path    => ['/bin/', '/usr/bin/'],
       command => 'rsync -avz --delete -e "ssh -o StrictHostKeyChecking=no" root@$remote_server:$remote_source $local_destination',
       user    => 'root',
     }
   } else {
+    exec { 'eval-ssh-agent':
+      path    => ['/bin/', '/usr/bin/'],
+      command => 'eval $(ssh-agent)',
+      user    => $run_as_user,
+    }
+    exec { 'add-ssh-agent':
+      path    => ['/bin/', '/usr/bin/'],
+      command => 'ssh-add',
+      user    => $run_as_user,
+    }
     exec { 'rsync-remote-dir':
       path    => ['/bin/', '/usr/bin/'],
       command => 'rsync -avz --delete -e "ssh -o StrictHostKeyChecking=no" $remote_user@$remote_server:$remote_source $local_destination',
